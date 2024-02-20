de1f1fa292a3f41dfb53cf4db3f74c40ff438484
riyazattri <riyaz.ansari@attri.ai>
2024-02-20 22:59:49 +0530

    initial commited

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,10 @@
+import { SignIn, SignUp } from "@clerk/nextjs";
+import React from "react";
+
+type Props = {};
+
+const SignInPage = (props: Props) => {
+  return <SignIn />;
+};
+
+export default SignInPage;
